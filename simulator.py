bd3ef46bda214710f7b6e021e9f737333ed30da8
Nadia Elvina Prameswari <nadiaelvina1012@gmail.com>
2026-06-12 14:30:36 +0700

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
 
 # ── Page config ──────────────────────────────────────────────────────────────
 st.set_page_config(
